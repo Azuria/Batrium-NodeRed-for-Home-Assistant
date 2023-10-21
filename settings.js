c39410a08307ce8d9b32e5b7c07bdd84f4ceed4f
DESKTOP-DE9QCGP\Azuria2 <mail@boegh.nu>
2023-10-21 02:40:13 +0200

    no message

---
+++ settings.js
@@ -0,0 +1,7 @@
+  functionGlobalContext: {
+    // os:require('os'),
+    // jfive:require("johnny-five"),
+    // j5board:require("johnny-five").Board({repl:false})
+    binary_parser:require('binary-parser').Parser,
+      
+  },
